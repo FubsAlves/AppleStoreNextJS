153a7d49fdeb62ea6fe30cf54422fa3489e0238e
Fabiano Alves <fabianoxtx@gmail.com>
2022-04-07 19:57:03 -0300

    Adição de icons no menu iPhone

--- src/pages/iphone.tsx
+++ src/pages/iphone.tsx
@@ -14,17 +14,17 @@ const Iphone: NextPage = () => {
           <Navbar/>
           <div className='flex'>
               <ul className='flex'>
-                  <li>iPhone 13 Pro</li>
-                  <li>iPhone 13</li>
-                  <li>iPhone SE</li>
-                  <li>iPhone 12</li>
-                  <li>iPhone 11</li>
-                  <li>Comparar</li>
-                  <li>AirPods</li>
-                  <li>AirTag</li>
-                  <li>Acessórios</li>
-                  <li>iOS 15</li>
-                  <li>Comparar iPhone</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_13_pro_light__bywz8u012wk2_large.svg'} alt='iPhone 13 Pro' height={54} width={38}/></figure>iPhone 13 Pro</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_13_light__ewo3e0sf67o2_large.svg'} alt='iPhone 13' height={54} width={36}/></figure>iPhone 13</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_se_light__fhg8duy6ffau_large.svg'} alt='iPhone SE' height={54} width={19}/></figure>iPhone SE</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_12_light__cxh2ll1zwpw2_large.svg'} alt='iPhone 12' height={54} width={36}/></figure>iPhone 12</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_11_light__b9p04p2r2vqu_large.svg'} alt='iPhone 11' height={54} width={22}/></figure>iPhone 11</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_compare_light__f4jj7brpbvm2_large.svg'} alt='Compare' height={54} width={45}/></figure>Comparar</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/airpods_light__8oj157p2476a_large.svg'} alt='AirPods' height={54} width={27}/></figure>AirPods</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/airtag_light__cb2bmnv6aoeu_large.svg'} alt='AirTag' height={54} width={30}/></figure>AirTag</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/accessories_light__ed5l6ipsevqu_large.svg'} alt='Accessories' height={54} width={35}/></figure>Acessórios</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/iphone_ios_light__b8s4ws8o77iq_large.svg'} alt='iOS 15' height={54} width={32}/></figure>iOS 15</li>
+                  <li><figure><Image src={'https://www.apple.com/v/iphone/home/be/images/chapternav/shop_iphone_light__b2toggskllle_large.svg'} alt='Compare iPhone' height={54} width={60}/></figure>Comparar iPhone</li>
               </ul>
           </div>
           <Footer/>
